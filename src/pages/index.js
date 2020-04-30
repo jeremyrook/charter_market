ca7bce3deb644ffc728b4f6d2d7a50f58dc1b577
Jeremy R <jeremy.rook01@gmail.com>
2020-04-29 20:19:50 -0500

    set up: fetch and filter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,58 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Col } from "reactstrap"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+export default class IndexPage extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      market : [],
+      searchValue: "",
+    };
+  }
 
-const IndexPage = () => (
-  <Layout>
+  async componentDidMount(){
+    const URL = "https://code-challenge.spectrumtoolbox.com/api/restaurants";
+    const response = await fetch(URL, {
+      headers: {
+        'Authorization': 'Api-Key q3MNxtfep8Gt', 
+      }
+    })
+    const data = await response.json();
+    this.setState({market: data});
+    console.log(this.state.market)
+  }
+
+  handleTextChange(e) {
+    this.setState({searchValue: e.target.value})
+  }
+
+  render(){
+    let marketList = this.state.market.filter(
+      (market) => {
+          return(
+            market.name.toLowerCase().includes(this.state.searchValue)
+            ) 
+      },
+    )
+    return(
+      <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Container">
+      <h1 className="mb-5 pb-4 font-weight-bold border-bottom text-center">Market</h1>
+      <Col className="text-center">
+            <input type="text" value={this.state.searchValue} onChange={this.handleTextChange.bind(this)}></input>
+            <div>
+              {marketList.map((market, i) => {
+                return <p key={i}>{market.name}</p>
+              })}
+            </div>
+            </Col>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
 
-export default IndexPage
+    )
+  }
+}
+
